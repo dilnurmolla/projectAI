dd2898ad1b3a3c4c5fbf250d57ddeb1b6832bc40
Dilnur Molla <dilnurmolla00@gmail.com>
2026-03-02 11:26:34 +0300

    all set

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{js,jsx}"],
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    DEFAULT: "#1B5CFF",
+                    hover: "#174EE0",
+                    from: "#1E62FF",
+                    to: "#174EE0",
+                },
+            },
+        },
+    },
+    plugins: [],
+};
